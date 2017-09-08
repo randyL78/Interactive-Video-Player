91c87344a76f09786ce177253f82c78bb595684c
RandyL <30959856+randyL78@users.noreply.github.com>
2017-09-07 21:48:11 -0400

    change from using mediaelement to custom player made by me

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/*jshint esversion: 6 */ 
+
+//  ================================================
+// 			Custom Video Player v1.0 alpha
+//				by: Randy Layne
+//	================================================
+
+document.addEventListener('DOMContentLoaded', () => {
+	//	global variables
+		// constants
+	const player = document.getElementsByClassName("custom_player");
+	const container = player.parentElement;
+	const skin = document.createElement("div");
+
+
+		// variables, set to defaults
+	let isFullScreen = false;
+	let ccMode = 1;				// default showing only one line at a time
+
+	// wrap video element in custom container
+	skin.className("mevp_skin");
+	
+	
+	// add custom controls to container
+
+	// implement closed captioning
+
+	// handle full screen mode
+
+	// allow users to change control colors
+});
